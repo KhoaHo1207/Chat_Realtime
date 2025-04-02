e871845629906cdf4bbf0111fa4f0c0f2c2e7f6f
Khoa <khoaho120703@gmail.com>
2025-04-02 11:41:34 +0700

    [Add]_Base strucre of project

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("Heheeh");
+};
+export const login = (req, res) => {
+  res.send("Heheeh");
+};
+export const logout = (req, res) => {
+  res.send("Heheeh");
+};
